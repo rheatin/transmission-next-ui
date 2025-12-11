df0fa17229d09fdfc0b91842262f8757a4f5e641
Char ONE <wang_char@apple.com>
2025-12-11 13:55:42 +0800

    🔧Fix: add missing hooks

--- src/components/torrent/table/ActionButton.tsx
+++ src/components/torrent/table/ActionButton.tsx
@@ -1,9 +1,9 @@
-import { IconDotsVertical, IconEdit, IconPlayerPlay, IconPlayerStop, IconTrash } from "@tabler/icons-react";
+import { IconDotsVertical, IconEdit, IconPlayerPlay, IconPlayerStop, IconTrash, IconFolderCheck } from "@tabler/icons-react";
 import { Button } from "@/components/ui/button.tsx";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu.tsx";
 import { useTranslation } from "react-i18next";
 import { Row } from "@tanstack/react-table";
-import { useStartTorrent, useStopTorrent } from "@/hooks/useTorrentActions.ts";
+import { useStartTorrent, useStopTorrent, useVerifyTorrent } from "@/hooks/useTorrentActions.ts";
 import { torrentSchema } from "@/schemas/torrentSchema.ts";
 import { RowAction } from "@/lib/utils/rowAction.ts";
 import { DialogType } from "@/lib/api/types.ts";
@@ -18,6 +18,7 @@ export function ActionButton({ row, setRowAction }: ActionButtonProps) {
     const { t } = useTranslation();
     const stopTorrent = useStopTorrent();
     const startTorrent = useStartTorrent();
+    const verifyTorrent = useVerifyTorrent();
 
     return (
             <DropdownMenu>
@@ -51,6 +52,10 @@ export function ActionButton({ row, setRowAction }: ActionButtonProps) {
                         </DropdownMenuItem>
                     )}
                     <DropdownMenuSeparator />
+                    <DropdownMenuItem onClick={() => verifyTorrent.mutate([row.original.id])}>
+                            <IconFolderCheck className="mr-2 h-4 w-4 text-blue-500" />
+                            {t("Verify")}
+                    </DropdownMenuItem>
                     <DropdownMenuItem variant="destructive" onSelect={() => {setRowAction({ dialogType: DialogType.Delete, targetRows: [row] })}}>
                         <IconTrash className="mr-2 h-4 w-4 text-red-500" />
                         {t("Delete")}
